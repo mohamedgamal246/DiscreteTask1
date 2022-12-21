391bf243f0f3dc6450158fa8982fdf58d9baeb14
mohamedgamal246 <121165901+mohamedgamal246@users.noreply.github.com>
2022-12-21 14:56:06 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+    int First, second, X, Y, sum;
+    Console.WriteLine("Enter the first");
+    First = int.Parse(Console.ReadLine());
+
+    Console.WriteLine("Enter the second");
+    second = int.Parse(Console.ReadLine());
+for (X = First; X <= second; X++)
+{
+    Y = 1;
+    sum = 0;
+    while (Y < X)
+    {
+        if (X % Y == 0)
+            sum = sum + Y;
+        Y++;
+    }
+    if (sum == X && X != 0)
+        
+        Console.WriteLine("{0}", X);
+}
