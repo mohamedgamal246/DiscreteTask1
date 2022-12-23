f51a99abdc63c2970a0db18b666f33f31d918e6e
mohamedgamal246 <121165901+mohamedgamal246@users.noreply.github.com>
2022-12-23 14:40:38 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,19 +1,29 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Enter a number:");
-int num = int.Parse(Console.ReadLine());
-int i = 0;
-bool x = true;
-for (i = 1; i <= 100; i++)
-{
-    for (int j = 2; j < i; j++)
-
-        if (i % j == 0)
+ 
+//Project 1
+// write a C# program to writ the prime numbers from ni to n2.
+//The Input: n1 and n2
+//The Output: The list of the prime numbers from ni to n2
+//Example: For the input numbers: n1 = 5 and n2 = 18, the output should be
+// 5 1/7 //11 //13 //17
+Console .WriteLine("enter n1");
+int n1 = int.Parse(Console.ReadLine());
+Console.WriteLine("enter n2");
+int n2 = int.Parse(Console.ReadLine());
+Console.WriteLine("the prime numbers between {0} and (1)", n1, n2);
+    for (int i = n1; i < -n2; i++)
+    {
+        int counter = 0;
+        for (int j = 2; j <= i / 2; j++)
+            if (i % j == 0)
+            {
+                counter++;
+                break;
+            }
+        if (counter == 0 && i != 1)
         {
-            bool X = false;
+            Console.WriteLine("(0}", i);
         }
 
-}
-if (x == true && i > 1)
-{
-    Console.WriteLine(" isprime\n");
-}
+    }
+    
